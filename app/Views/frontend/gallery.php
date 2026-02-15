72d76697ee78f41f2da18268839040523e18475b
Nisam Git <nisamvp10@gmail.com>
2026-02-15 13:05:14 +0530

    gallery

--- app/Views/frontend/gallery.php
+++ app/Views/frontend/gallery.php
@@ -1,35 +1,74 @@
 <?= view('frontend/inc/header') ?>
-<link rel="stylesheet" href="<?= base_url('public/assets/template/') ?>css/magnific-popup.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
 
 
-	<div class="page-header bg-section dark-section">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<!-- Page Header Box Start -->
-					<div class="page-header-box">
-						<h1 class="text-anime-style-3" data-cursor="-opaque">
-                         Gallery
+     <!-- Search Area Start -->
+      <div class="search-wrap">
+         <div class="search-inner">
+            <i class="fas fa-times search-close" id="search-close"></i>
+            <div class="search-cell">
+               <form method="get">
+                  <div class="search-field-holder">
+                     <input
+                        type="search"
+                        class="main-search-input"
+                        placeholder="Search..."
+                     />
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
 
-                         </h1>
-						<nav class="wow fadeInUp">
-							<ol class="breadcrumb">
-								<li class="breadcrumb-item"><a href="#">home</a></li>
-								<li class="breadcrumb-item active" aria-current="page">Gallery</li>
-							</ol>
-						</nav>
-					</div>
-					<!-- Page Header Box End -->
-				</div>
-			</div>
-		</div>
-	</div>
-
+      <div class="breadcumb-section">
+        <div
+           class="breadcumb-container-wrapper"
+           data-bg-src="<?=base_url('public/assets/template/');?>assets/images/breadcumb/breadcumb-bg.png"
+        >
+           <div class="shape1">
+              <img
+                 src="<?=base_url('public/assets/template/');?>assets/img/breadcumb-shape1_1.png"
+                 alt="shape"
+              />
+           </div>
+           <div class="shape2">
+              <img
+                 src="<?=base_url('public/assets/template/');?>assets/img/breadcumb-shape1_2.png"
+                 alt="shape"
+              />
+           </div>
+           <div class="shape3">
+              <img
+                 src="<?=base_url('public/assets/template/');?>assets/img/breadcumb-shape1_3.png"
+                 alt="shape"
+              />
+           </div>
+           <div class="shape4">
+              <img
+                 src="<?=base_url('public/assets/template/');?>assets/img/breadcumb-shape1_4.png"
+                 alt="shape"
+              />
+           </div>
+           <div class="container">
+              <ul class="breadcumb-wrapper">
+                 <li>
+                    <a href="index.html"
+                       ><i class="fa-sharp fa-light fa-house"></i
+                    ></a>
+                 </li>
+                 <li><i class="fa-solid fa-chevron-right"></i></li>
+                 <li>Home</li>
+                 <li><i class="fa-solid fa-chevron-right"></i></li>
+                 <li>Gallery</li>
+              </ul>
+           </div>
+        </div>
+     </div>
 
 
 
     <!-- Page Contact Us Start -->
-    <div class="page-gallery">
+    <div class="page-gallery pt-4 pb-4">
         <div class="container">
             <!-- gallery section start -->
             <div class="row gallery-items page-gallery-box">
@@ -40,7 +79,7 @@
                      <div class="col-lg-4 col-6">
                     <!-- Image Gallery start -->
                     <div class="photo-gallery wow fadeInUp">
-                        <a href="<?=validImg($img['image']) ?> ?>" data-cursor-text="View">
+                        <a href="<?=validImg($img['image']) ?>" data-fancybox="gallery">
                             <figure class="image-anime">
                                 <img src="<?= validImg($img['image']) ?>" alt="">
                             </figure>
@@ -63,6 +102,17 @@
     <!-- Page Contact Us End -->
 
    <?= view('frontend/inc/footerLink') ?>
+   <!-- Fancybox JS -->
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.umd.js"></script>
+<script>
+    Fancybox.bind("[data-fancybox='gallery']", {
+        infinite: true,
+        Thumbs: {
+            autoStart: true,
+        },
+    });
+</script>
+
 </body>
 
 </html>
