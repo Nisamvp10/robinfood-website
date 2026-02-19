c560baf2545f15108714972a86381203c9257355
Nisam Git <nisamvp10@gmail.com>
2026-02-19 15:31:06 +0530

    add product

--- app/Views/template/layout/main.php
+++ app/Views/template/layout/main.php
@@ -195,7 +195,7 @@ if (!empty($userData)){
                 <li class="nav-item">
                     <a class="nav-link <?= strpos(uri_string(), 'admin/all-products') === 0  ? 'active' : '' ?>" href="<?= base_url('admin/all-products') ?>">
                     <span class="flex-shrink-0"><svg xmlns="https://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles "><path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z"></path><path d="M5 3v4"></path><path d="M19 17v4"></path><path d="M3 5h4"></path><path d="M17 19h4"></path></svg></span>
-                        <span>Investments</span>
+                        <span>Add Product</span>
                     </a>
                 </li>
                 <?php  
