5b57064c5be7fe4f92dc5822a2a6fd0909ad0061
Nisam Git <nisamvp10@gmail.com>
2026-02-19 15:34:30 +0530

    change product title placeholder

--- app/Views/modal/productMnagementModal.php
+++ app/Views/modal/productMnagementModal.php
@@ -45,7 +45,7 @@
                             <label class="block text-sm font-medium text-gray-700 mb-1"> Title *</label>
                             <div class="relative">
                                 <div class="absolute inset-y-0 left-0 pl-3 mt-2 items-center pointer-events-none"><i class="bi bi-briefcase text-xl text-gray-400"></i></div>
-                                <input type="text" name="title"  id="title" class="pl-10 pr-3 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500" placeholder="Enter Your News Title">
+                                <input type="text" name="title"  id="title" class="pl-10 pr-3 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500" placeholder="Enter Your Product Title">
                                 <div class="invalid-feedback" id="title_error"></div>
                             </div>
                         </div>
